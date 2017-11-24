9cf5b48e2f234ac425b0dfc199f9fc3a4c990cad
MipsaPatel <mipsappatel@gmail.com>
2017-11-24 23:04:56 +0530

    RNN class added.

---
+++ rnn.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+from torch.autograd import Variable
+
+class RNN(nn.Module):
+    def __init__(self, input_size, num_layers, num_classes, hidden_size):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_layers)
+
+
+    def forward(self, x):
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+        c0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size))
+
+        out, _ = self.lstm(x, (h0, c0))
+
+        out = self.fc(out[:, -1, :])
+        return out
