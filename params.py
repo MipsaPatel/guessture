dbc6c5bac47c542ebbe7c3c2e59d87ab758ca44a
TilakSN <tilakchinnu96@gmail.com>
2017-12-28 01:31:23 +0530

    Updated the function from Scale to Resize

--- params.py
+++ params.py
@@ -30,7 +30,7 @@ class Parameters:
         self.transforms = transforms.Compose([
             transforms.ToPILImage(),                        # for crop and scale
             transforms.CenterCrop((240, 240)),              # change resolution, eliminate sides
-            transforms.Scale(128),                          # scale it down
+            transforms.Resize(128),                          # scale it down
             transforms.Lambda(lambda x: x.convert('L')),    # gray scale
             transforms.ToTensor()                           # make it easier for pyTorch
         ])
